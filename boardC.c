580a8557d0fadd96947ec2532c184072ef47059e
Artur Alkhimovich <realtura@gmail.com>
2018-04-15 19:43:56 +0700

    Add a file creating a board

---
+++ boardC.c
@@ -0,0 +1,10 @@
+#include "board.h"
+
+void boardCreate(char A[][9])
+{
+  FILE *f = fopen("table.txt", "rt");
+  int i = 0, j = 0;
+  for(i = 0; i < 9; i++)
+    for(j = 0; j < 9; j++)
+      fscanf(f, "%c", &A[j][i]);
+}
