d441c41d578a091fb3fac4331bdf971a9ce58c10
Альхимович Артур Павлович <ip713s02@bwp17.csc.local>
2018-04-16 14:00:07 +0700

    Add include guard

--- src/board.h
+++ src/board.h
@@ -1,10 +1,11 @@
+#ifndef BOARD_H
 #define BOARD_H
 
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <guard.h>
 
 void boardCreate(char A[][9]);
 void boardPrint(char A[][9]);
 void boardMove(char A[][9]);
+#endif
